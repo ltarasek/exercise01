295f781f522d3d3d3951bf9695ecb07d8ac39476
mtarasek <mtarasek@gmail.com>
2019-01-09 19:15:44 +0100

    added function add

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ window.onload = () => {
 
 }
 
+function add() {
+
+    var newName = $('input#name');
+    var name = newName.val();
+
+    var newSurname = $('input#surname');
+    var surname = newSurname.val();
+
+    var person = {
+        name: name,
+        surname: surname
+    }
+    allPersons.push(person);
+
+    generateList(allPersons);
+
+    newName.val('');
+    newSurname.val('');
+
+}
+
 function generateList(tab) {
 
 }
