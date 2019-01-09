38897c174cea1c9fbc944f50f7270a7290efcb9c
ltarasek eco <ltarasek@gmail.com>
2019-01-09 19:17:44 +0100

    set up function generateList() and added to window.onloade

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ var allPersons = [{
 }];
 
 window.onload = () => {
-
+    
+    generateList(allPersons);
 }
 
 function generateList(tab) {
-
+    for (let i = 0; i < tab.length; i++) {
+        var $tr = $('<tr><td>' + tab[i].name + '</td><td>' + tab[i].surname + '</td></tr>');
+        $('#tbody').append($tr);
+    }
 }
+
