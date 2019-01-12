6d47f43b2499b4c1e2c04c6997c1b469dcba00cb
mtarasek <mtarasek@gmail.com>
2019-01-12 19:06:42 +0100

    add to LocalStorage

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ var allPersons = [{
 }];
 
 window.onload = () => {
-    
+
+    addToLocalstorage();
+
     generateList(allPersons);
 }
 
+function addToLocalstorage() {
+    var JsonAllPerson = JSON.stringify(allPersons);
+    localStorage.setItem("tablicaZosobami", JsonAllPerson);
+    console.log(JsonAllPerson);
+}
 function add() {
 
     var newName = $('input#name');
@@ -23,6 +30,7 @@ function add() {
     allPersons.push(person);
 
     generateList(allPersons);
+    addToLocalstorage();
 
     newName.val('');
     newSurname.val('');
@@ -30,6 +38,7 @@ function add() {
 }
 
 function generateList(tab) {
+    $('#tbody').html('');
     for (let i = 0; i < tab.length; i++) {
         var $tr = $('<tr><td>' + tab[i].name + '</td><td>' + tab[i].surname + '</td></tr>');
         $('#tbody').append($tr);
